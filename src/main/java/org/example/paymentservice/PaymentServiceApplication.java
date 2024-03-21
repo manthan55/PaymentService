b90e95c2214721aa9eee8c32d8d22b92ffa0aeab
Manthan Patidar <manthanpatidar@gmail.com>
2024-03-21 13:05:19 +0530

    configuring spring to run even without DB availability

--- src/main/java/org/example/paymentservice/PaymentServiceApplication.java
+++ src/main/java/org/example/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,13 @@ package org.example.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//https://stackoverflow.com/a/38150900
+//https://stackoverflow.com/a/69464593
+// this will make spring run even without DB being available
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+//@SpringBootApplication
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
