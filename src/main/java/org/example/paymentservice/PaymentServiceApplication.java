ab6ab258bbaf65370176ed3c725176000472e1df
Manthan Patidar <manthanpatidar@gmail.com>
2024-03-25 16:24:53 +0530

    completely switching to sqlite in all envs

--- src/main/java/org/example/paymentservice/PaymentServiceApplication.java
+++ src/main/java/org/example/paymentservice/PaymentServiceApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 //https://stackoverflow.com/a/38150900
 //https://stackoverflow.com/a/69464593
 // this will make spring run even without DB being available
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-//@SpringBootApplication
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
